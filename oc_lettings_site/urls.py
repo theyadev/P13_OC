d2e3bbd9307744711f03891dd32ea1c5fdc29373
Théo FASSI <tfa@subteno.com>
2023-01-10 16:33:45 +0100

    fix: lint sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 
 from . import views
 
+
 def trigger_error(request):
     division_by_zero = 1 / 0
+
+    return division_by_zero
+
 
 urlpatterns = [
     path('', views.index, name='index'),
