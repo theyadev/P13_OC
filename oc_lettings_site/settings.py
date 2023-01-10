586a5be595dff119a7259d12fc74871563973d53
Fassi Théo <fassitheo@protonmail.com>
2023-01-10 16:35:00 +0100

    feature: added sentry
    * feature: added sentry
    
    * fix: lint sentry
    
    Co-authored-by: Théo FASSI <tfa@subteno.com>

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,26 @@
 import os
+import sentry_sdk
+
+from dotenv import load_dotenv
+from sentry_sdk.integrations.django import DjangoIntegration
+
+load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN'),
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
