6f737b34b3a84bd3b1d313071230c4d96fc69cfc
Théo FASSI <tfa@subteno.com>
2023-01-10 16:32:28 +0100

    feature: added sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 
 from . import views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
+    path('sentry-debug/', trigger_error),
 ]
